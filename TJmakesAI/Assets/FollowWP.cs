e345951fb9bc143fe19260fd0b0ee622b9c262ee
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2025-01-08 00:51:17 -0700

    ONE MORE
    WHOO HOO

--- TJmakesAI/Assets/FollowWP.cs
+++ TJmakesAI/Assets/FollowWP.cs
@@ -7,68 +7,45 @@ public class FollowWP : MonoBehaviour
     public GameObject[] waypoints;
     int currentWP = 0;
 
-    GameObject tracker;
-
     public float speed = 10.0f;
-    public float rotSpeed = 10.0f;
+    public float rotspeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (waypoints.Length > 0)  // Ensuring there are waypoints to follow
-        {
-            tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            DestroyImmediate(tracker.GetComponent<Collider>());
-            tracker.transform.position = this.transform.position;
-            tracker.transform.rotation = this.transform.rotation;
-        }
-        else
-        {
-            Debug.LogWarning("No waypoints assigned!");
-        }
-        Debug.Log("Waypoints assigned: " + waypoints.Length);  // Debugging number of waypoints
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
+
 
     void ProgressTracker()
     {
-        if (waypoints.Length == 0)
-        {
-            return;  // If there are no waypoints, do nothing
-        }
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
 
-        // Check the distance to the current waypoint
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3f)
-        {
-            currentWP++;  // Increment after checking the condition
-        }
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
 
-        // Check if currentWP is out of bounds, reset it to 0 if it is
         if (currentWP >= waypoints.Length)
-        {
             currentWP = 0;
-        }
 
-        // Make the tracker look at the next waypoint
         tracker.transform.LookAt(waypoints[currentWP].transform);
-
-        // Move the tracker towards the next waypoint based on speed
-        float step = speed * Time.deltaTime;  // Calculate movement step
-        tracker.transform.Translate(0, 0, step);  // Move the tracker
+        tracker.transform.Translate(0, 0, (speed + 20) * Time.deltaTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
         ProgressTracker();
 
-        // Rotate the object towards the next waypoint based on rotSpeed
-        if (waypoints.Length > 0)
-        {
-            Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
-        }
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
 
-        // Move the object based on speed (if needed)
-        // this.transform.Translate(0, 0, speed * Time.deltaTime);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotspeed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
